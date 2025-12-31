11b58fb20aad98809039ce10b0743c148591a688
ducng99 <tom@tomng.dev>
2026-01-01 01:27:56 +1300

    refactor: use new manager packages

---
+++ src/libs/shortlinks/manager.ts
@@ -0,0 +1,30 @@
+import { createManager, type IShortLinksManager } from "@potonz/shortlinks-manager";
+import { createD1Backend } from "@potonz/shortlinks-manager-cf-d1";
+import { createServerOnlyFn } from "@tanstack/solid-start";
+import { env } from "cloudflare:workers";
+
+let manager: IShortLinksManager;
+let shortIdLength = 3;
+
+export const getShortLinksManager = createServerOnlyFn(async () => {
+    if (!manager) {
+        const storedLength = await env.CONFIG.get("short_id_length");
+        shortIdLength = storedLength ? parseInt(storedLength) : 3;
+
+        manager = await createManager({
+            backend: createD1Backend(env.DB),
+            shortIdLength,
+            async onShortIdLengthUpdated(newLength) {
+                shortIdLength = newLength;
+                try {
+                    await env.CONFIG.put("short_id_length", newLength.toString());
+                }
+                catch (err) {
+                    console.warn("Failed when updating KV short_id_length", err);
+                }
+            },
+        });
+    }
+
+    return manager;
+});
