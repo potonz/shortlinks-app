713c191e1f03e9cac0a0489ff5cb180301c3421a
ducng99 <tom@tomng.dev>
2026-01-02 02:24:54 +1300

    feat: added caching target url using Cloudflare KV

--- src/libs/shortlinks/getTargetUrl.ts
+++ src/libs/shortlinks/getTargetUrl.ts
@@ -1,8 +1,21 @@
 import { createServerOnlyFn } from "@tanstack/solid-start";
+import { env } from "cloudflare:workers";
 
 import { getShortLinksManager } from "./manager";
 
 export const getTargetUrl = createServerOnlyFn(async (shortId: string) => {
+    const cachedUrl = await env.SHORTLINKS_CACHE.get(shortId);
+
+    if (cachedUrl) {
+        return cachedUrl;
+    }
+
     const manager = await getShortLinksManager();
-    return await manager.getTargetUrl(shortId);
+    const targetUrl = await manager.getTargetUrl(shortId);
+
+    if (targetUrl) {
+        await env.SHORTLINKS_CACHE.put(shortId, targetUrl, { expirationTtl: 31_536_000 });
+    }
+
+    return targetUrl;
 });
