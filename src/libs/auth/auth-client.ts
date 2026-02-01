21dc75b1927160ad6b0a157993430bf6316537b0
ducng99 <tom@tomng.dev>
2026-02-01 13:28:49 +1300

    fixed env variables

--- src/libs/auth/auth-client.ts
+++ src/libs/auth/auth-client.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/solid";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+    baseURL: process.env.BETTER_AUTH_BASE_URL,
+});
 
 export type Session = typeof authClient.$Infer.Session;
