b897d77a02c2489e5a0a9ac8618e1c3bb740203d
ducng99 <tom@tomng.dev>
2026-02-25 01:31:42 +1300

    fix: removed static base url fetching
    cannot fetch during build

--- src/libs/shortlinks/getBaseUrls.functions.ts
+++ src/libs/shortlinks/getBaseUrls.functions.ts
@@ -1,12 +1,16 @@
+import type { IBaseUrlRecord } from "@potonz/shortlinks-manager";
 import { createServerFn } from "@tanstack/solid-start";
-import { staticFunctionMiddleware } from "@tanstack/start-static-server-functions";
 
 import { getShortLinksManager } from "~/libs/shortlinks/manager";
 
+let cached: IBaseUrlRecord[] | null = null;
+
 export const getBaseUrls = createServerFn({ method: "GET" })
-    .middleware([staticFunctionMiddleware])
     .handler(async () => {
-        const manager = await getShortLinksManager();
-        const baseUrls = await manager.baseUrl.list(true);
-        return baseUrls;
+        if (!cached) {
+            const manager = await getShortLinksManager();
+            cached = await manager.baseUrl.list(true);
+        }
+
+        return cached;
     });
